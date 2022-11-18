c8476cc29f13a319b8baf5ebe68e7b5ac731a448
gikoo <gikoo@aliyun.com>
2022-11-18 12:01:51 +0800

    gtest环境搭建

--- test/gistring_test.cpp
+++ test/gistring_test.cpp
@@ -1,10 +1,6 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions)
+int main(int, char **)
 {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+    return 0;
 }
